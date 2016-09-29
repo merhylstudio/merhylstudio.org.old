b49bbbcf8618b391bf789e32efde471d9de82a2c
Maghin <maghin@devcentos7.merhylstudio.org>
2016-09-29 19:30:45 +0200

    modified:   lib/capistrano/restart.rb 	modified:   web/index.php 	modified:   web/view/css/style.css 	modified:   web/view/img/merhylstudio.png

--- lib/capistrano/restart.rb
+++ lib/capistrano/restart.rb
@@ -12,7 +12,7 @@ namespace :merhyl do
       on roles(:web), in: :sequence, wait: 5 do
         # Restart mechanism here
         execute :touch, release_path.join('tmp/restart.txt')
-        run 'systemctl restart httpd'
+        run 'sudo apachectl restart'
     end
   end
 end
--- web/index.php
+++ web/index.php
@@ -1,72 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
-  <meta name="viewport" content="width=device-width, initialscale=1" />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-  <title>MerhylStudio.org</title>
-
-  <link rel="stylesheet" href="view/css/bootstrap.min.css" type="text/css"  />
-  <link rel="stylesheet" href="view/css/style.css" type="text/css" />
-
-  <script src="view/js/jquery-1.11.3-jquery.min.js"></script>
-  <script src="view/js/bootstrap.min.js"></script>
-
+<title>Accueil</title>
+<?php include("view/header.php"); ?>
 </head>
-
-<body>
-
+<body id="topPage">
 <!-- navbar -->
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="http://www.merhylstudio.org">MerhylStudio.org</a>
-    </div>
-
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="signin.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
-        <li><a href="signin.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-      </ul>
-    </div>
-
-  </div>
-</nav>
-
-
+<?php include("view/navbar.php"); ?>
+<!-- infos -->
+<?php include("view/topbar.php"); ?>
 <!-- corps -->
-<div class="container-fluid text-center">
-  <img class="img-responsive" style="display:inline" src="view/img/merhylstudio.png" alt="merhylstudio" />
-
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <h1>En construction...</h1><br /><br /><br /><br /><br /><br /><br /><br /><br />
+<div class="container-fluid bg-lightblue">
+  <div class="text-center">
+      <img class="img-responsive img-rounded bg-nightblue" style="display:inline" src="view/img/merhylstudio.png" alt="merhylstudio" />
+    <h1>En construction...</h1>
+  </div>
 </div>
-
-
 <!-- footer -->
-
-
+<?php include("view/footer.php"); ?>
+<!-- JavaScript -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- Script pour "smooth scroll" -->
+<script type="text/javascript" src="view/js/smoothscroll.js"></script>  
 </body>
 </html>
